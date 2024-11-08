cd04697a5dded7d6cd9a95e39943cf44a5ec1491
MariaVictoria1997 <netvista.vito@hotmail.com>
2024-11-08 09:28:14 +0100

    08-11-2024

---
+++ github.bat
@@ -0,0 +1,11 @@
+git init
+git config --unset user.name
+git config --unset user.email
+git config user.name "MariaVictoria1997"
+git config user.email "netvista.vito@hotmail.com"
+git remote set-url origin https://github.com/MariaVictoria1997/portfolio.git
+git remote -v
+git config --local --list
+git add .
+git commit -m "08-11-2024"
+git push origin main
